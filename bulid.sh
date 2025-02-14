3973177b0e689d6c0287f27fb6703c7e1966a161
Rakesh <149141900+Rakesh-o@users.noreply.github.com>
2025-02-14 11:03:01 +0530

    Delete bulid.sh

--- bulid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install system dependencies for PyAudio
-apt-get update && apt-get install -y portaudio19-dev pulseaudio alsa-utils
-
-# Install Python dependencies
-pip install --no-cache-dir -r requirements.txt
